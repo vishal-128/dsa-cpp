a192dac584cdd7ff5e8829fbe2170115ac730b6b
vishal <garg01.vishal@gmail.com>
2022-12-09 02:40:48 +0530

    changes

--- pattern2.cpp
+++ pattern2.cpp
@@ -18,6 +18,6 @@ int main()
         {
             cout<<"*";
         }
-        cout<<endl;
+        cout<< endl;
     }
 }
--- rotatearray.cpp
+++ rotatearray.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 int main()
 {
-    int a[4]={10,20,30,40};
-    int k=3,n=4;
-    int b[4],temp,x;
+    int a[8]={10,20,30,40,50,60,70,80};
+    int k=3,n=8;
+    int b[8],temp,x;
     for(int i=0;i<n;i++)
     {
         
-      if ((i+k)<4)
+      if ((i+k)<n)
       {
         x=i+k;
         temp=a[i];
@@ -17,7 +17,7 @@ int main()
       } 
       else
       {
-        x=(i+k)-4;
+        x=(i+k)-n;
         temp=a[i];
         b[x]=temp;
 
